89e42e879afa032d7da249300177b6a8a209f591
node01 user <node01user@noemail.com>
2016-10-31 15:47:51 +0000

    added puppet instructions to created user: art and added vagrant public key to art's authorized_users file

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,5 +1,26 @@
 node 'node01' {
+
   include nginx
+
+  user { 'art':
+    ensure 		=> present,
+    comment		=> 'Art Vandeley',
+    home 		=> '/home/art',
+    managehome 	=> true,			##ensures that home dir is created (if required) 
+  }
+
+  ## Adds the specified public key to art's authorized_users file (art is the name of a user)
+  ## Recap: this allows any user with the associated private key to ssh onto this puppet node as user: art
+  ssh_authorized_key { 'art_ssh':
+    user		=> 'art',
+    type		=> 'rsa',
+    key 		=> 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDO/I8pygUsouZkYcwaeneroXiTli7ZoM4PeVdC1CcJfizyAIeN3GTUNO6hmixNW7b1TctwPrruXDQLoWK6FLtJCx1mNVmt3VTEMcqL9brED1c61K3twaqWlbsOTwUThYYcPAZpDV20hQSetXvi3RnbFYqDc/y4hd2y1nojDhBhOy6pFHU4EHV4uyfSYt0ENN8cMVaZ9G0TlrBtHruRTT29aw0UDSA+nOndJetginLst86gtDzTUoWyGLvOmGfORzH3m8yj+jmB+TD9YhvEu6KUxZS98kB3AxSlyN8VP+6QmiTEwNFy3uEr8DuQGagZD+np1e+uTZ+PtH+ZlG03uQ4r',
+  }
+
+  #user { 'art':
+  #  ensure 	=> absent,
+  #}
+
 }
 
 node 'node02' {
