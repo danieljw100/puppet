d1513f1d3399116ac7d8e84204b226f408270724
Daniel Wilkie <dan@danielwilkie.com>
2016-11-17 10:02:56 +0000

    docker class almost finished

--- modules/docker/manifests/init.pp
+++ modules/docker/manifests/init.pp
@@ -2,13 +2,35 @@
 
 class docker {
 
-  package { 'docker':
+  file { '/etc/apt/sources.list.d/docker.list':
+    source => 'puppet:///modules/docker/docker.list',
+  } 
+
+  package { "linux-image-extra-$kernelrelease":
     ensure => installed,
+  }
+
+  package { 'linux-image-extra-virtual':
+    ensure => installed,
+  }
+
+  package { 'docker.io':
+    ensure => installed,
+    require => [ File['/etc/apt/sources.list.d/docker.list'], Package["linux-image-extra-$::kernelrelease"], Package['linux-image-extra-virtual'] ],
   }
 
   service { 'docker':
     ensure => running,
-    require => Package['docker'],
+    require => Package['docker.io'],
   } 
+
+  group { 'docker':
+    ensure => present,
+  }
+
+  user { 'vagrant':
+    groups => ['docker'],
+    require => Group['docker'],
+  }
 
 }
